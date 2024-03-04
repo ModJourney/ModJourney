62c709e9176efe8aaa0731d09fba4d3f06ad98aa
zukkl <mrozikrobin@gmail.com>
2024-03-04 15:51:00 +0100

    Titel und kubejs/client_scripts

--- overrides/kubejs/client_scripts/example.js
+++ overrides/kubejs/client_scripts/example.js
@@ -1,6 +1,8 @@
 // priority: 0
 
 // Visit the wiki for more info - https://kubejs.com/
-
+// Zum deaktivieren von JEI Rezepten, folgender Link: https://wiki.latvian.dev/books/kubejs-legacy/page/jei-integration
+// Zusatz zur Item entfernung: https://kubejs.com/support/tickets/0BE90DD0
+// weiterer Zusatz https://www.reddit.com/r/feedthebeast/comments/vc5xbq/using_kubejs_to_hide_items_in_jei_that_have_nbt/
 console.info('Hello, World! (Loaded client scripts)')
 
