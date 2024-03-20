c56a2015074582ecf8c5301bc029363a38271af2
Radon8472 <develop@radon-software.net>
2024-03-20 07:44:25 +0100

    Run build-script always relative to project location -> script can be used from everywhere now

--- dev-tools/build-client-archive.sh
+++ dev-tools/build-client-archive.sh
@@ -8,6 +8,13 @@
 #
 
 echo "Building client archive"
-ARCHIV_FILE_NAME=dytech-4-client.zip
-mkdir -p build/
-git archive --add-file=manifest.json --add-file=modlist.html --prefix=overwrites/ -o build/${ARCHIV_FILE_NAME} HEAD
+ARCHIVE_FILE_NAME=dytech-4-client.zip
+SCRIPT_DIR="$(dirname "$0")"
+PROJECT_DIR="${SCRIPT_DIR}/../"
+BUILD_DIR="${PROJECT_DIR}build/"
+
+mkdir -p "${BUILD_DIR}"
+git archive --add-file="${PROJECT_DIR}manifest.json" --add-file="${PROJECT_DIR}modlist.html" --prefix=overwrites/ -o "${BUILD_DIR}${ARCHIVE_FILE_NAME}" HEAD
+
+# Show the created file
+ls -lh "${BUILD_DIR}${ARCHIVE_FILE_NAME}"
