7c7d5628fefb7093dcae304c23afab12b720be22
Radon8472 <develop@radon-software.net>
2024-03-25 22:13:15 +0100

    Fix running the buildscript from different working directory
    - also fixes the wrong directory name (in the archive) from "overwrites" to "overwrides"

--- dev-tools/build-client-archive.sh
+++ dev-tools/build-client-archive.sh
@@ -11,10 +11,12 @@ echo "Building client archive"
 ARCHIVE_FILE_NAME=dytech-4-client.zip
 SCRIPT_DIR="$(dirname "$0")"
 PROJECT_DIR="${SCRIPT_DIR}/../"
-BUILD_DIR="${PROJECT_DIR}build/"
+BUILD_DIR=$(realpath "${PROJECT_DIR}build/")
 
-mkdir -p "${BUILD_DIR}"
-git archive --add-file="${PROJECT_DIR}manifest.json" --add-file="${PROJECT_DIR}modlist.html" --prefix=overwrites/ -o "${BUILD_DIR}${ARCHIVE_FILE_NAME}" HEAD
+mkdir -p "${BUILD_DIR}/"
+cd "$PROJECT_DIR" &&
+  git archive --add-file="manifest.json" --add-file="modlist.html" --prefix=overrides/ -o "${BUILD_DIR}/${ARCHIVE_FILE_NAME}" HEAD &&
+  cd - || echo "Could restore working directory"
 
 # Show the created file
-ls -lh "${BUILD_DIR}${ARCHIVE_FILE_NAME}"
+ls -lh "${BUILD_DIR}/${ARCHIVE_FILE_NAME}"
