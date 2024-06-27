6b8ef4cdeea403de98fe955ac0d0077d51652449
Radon8472 <develop@radon-software.net>
2024-06-27 22:49:38 +0200

    Reformat code to match gitattributes

--- kubejs/client_scripts/jei_hide.js
+++ kubejs/client_scripts/jei_hide.js
@@ -2,196 +2,196 @@
 
 JEIEvents.hideItems(event => {
 
-  //thermal series tnt removen
+    //thermal series tnt removen
 
-  const tnt = [
-    'thermal:detonator',
-    'thermal:explosive_grenade',
-    'thermal:fire_tnt',
-    'thermal:ice_tnt',
-    'thermal:lightning_tnt',
-    'thermal:earth_tnt',
-    'thermal:ender_tnt',
-    'thermal:glowstone_tnt',
-    'thermal:redstone_tnt',
-    'thermal:slime_tnt',
-    'thermal:phyto_tnt',
-    'thermal:nuke_tnt'
+    const tnt = [
+        'thermal:detonator',
+        'thermal:explosive_grenade',
+        'thermal:fire_tnt',
+        'thermal:ice_tnt',
+        'thermal:lightning_tnt',
+        'thermal:earth_tnt',
+        'thermal:ender_tnt',
+        'thermal:glowstone_tnt',
+        'thermal:redstone_tnt',
+        'thermal:slime_tnt',
+        'thermal:phyto_tnt',
+        'thermal:nuke_tnt'
     ]
-    
+
     tnt.forEach(element => {
-      event.hide(element)
+        event.hide(element)
     });
-  
-  //thermal series hide florb
-  event.hide('thermal:florb')
-  
-  //botania lenses removen
 
-  const lens = [
-    'botania:lens_explosive',
-    'botania:lens_piston',
-    'botania:lens_weight',
-    'botania:lens_mine'
-  ]
+    //thermal series hide florb
+    event.hide('thermal:florb')
 
-  lens.forEach(element => {
-    event.hide(element)
-  });
+    //botania lenses removen
 
+    const lens = [
+        'botania:lens_explosive',
+        'botania:lens_piston',
+        'botania:lens_weight',
+        'botania:lens_mine'
+    ]
 
-  //botania Staebe removen
-
-  let rods = [
-    'botania:terraform_rod',
-    'botania:exchange_rod',
-    'botania:water_rod'
-  ]
-
-  rods.forEach(element => {
-    event.hide(element)
-  });
-
-  //botania Laputa Shard removen
-  event.hide('botania:laputa_shard')
+    lens.forEach(element => {
+        event.hide(element)
+    });
 
 
-  // Mystical Agriculture
-  let mystical_hide = [
-    'mysticalagriculture:harvester'
-  ]
+    //botania Staebe removen
 
-  // remove all growth accelerators (but keep the first)
-  let mystical_tiers = [
-    // inferium,
-    'prudentium',
-    'tertium',
-    'imperium',
-    'supremium'
-  ];
+    let rods = [
+        'botania:terraform_rod',
+        'botania:exchange_rod',
+        'botania:water_rod'
+    ]
 
-  // Add accelerators to remove list
-  mystical_tiers.forEach(tier => {
-    mystical_hide.push(Item.of(`mysticalagriculture:${tier}_growth_accelerator`))
-  })
+    rods.forEach(element => {
+        event.hide(element)
+    });
 
-  mystical_hide.forEach((element) => {
-    event.hide(element)
-  })
+    //botania Laputa Shard removen
+    event.hide('botania:laputa_shard')
 
 
-  //immersive
-  event.hide('immersiveengineering:cloche')
+    // Mystical Agriculture
+    let mystical_hide = [
+        'mysticalagriculture:harvester'
+    ]
+
+    // remove all growth accelerators (but keep the first)
+    let mystical_tiers = [
+        // inferium,
+        'prudentium',
+        'tertium',
+        'imperium',
+        'supremium'
+    ];
+
+    // Add accelerators to remove list
+    mystical_tiers.forEach(tier => {
+        mystical_hide.push(Item.of(`mysticalagriculture:${ tier }_growth_accelerator`))
+    })
+
+    mystical_hide.forEach((element) => {
+        event.hide(element)
+    })
 
 
-  //Mob_Grinding_Utils
-  const mob_grinding = [
-    'mob_swab_used',
-    'ender_inhibitor_off',
-    'dreadful_dirt',
-    'delightful_dirt',
-    'rotten_egg',
-    'golden_egg',
-    'monocle',
-    'nutritious_chicken_feed',
-    'gm_chicken_feed_cursed',
-    'dark_oak_stone',
-  ]
-
-  mob_grinding.forEach((element) => {
-    event.hide(Item.of('mob_grinding_utils:' + element).ignoreNBT())
-  });
+    //immersive
+    event.hide('immersiveengineering:cloche')
 
 
-  //Structure_gel API Build Mod
-  const structure_api = [
-    'dynamic_spawner',
-    'red_gel',
-    'blue_gel',
-    'green_gel',
-    'cyan_gel',
-    'orange_gel',
-    'building_tool',
-    'yellow_gel',
-    'data_handler'
-  ]
+    //Mob_Grinding_Utils
+    const mob_grinding = [
+        'mob_swab_used',
+        'ender_inhibitor_off',
+        'dreadful_dirt',
+        'delightful_dirt',
+        'rotten_egg',
+        'golden_egg',
+        'monocle',
+        'nutritious_chicken_feed',
+        'gm_chicken_feed_cursed',
+        'dark_oak_stone',
+    ]
 
-  structure_api.forEach((element) => {
-    event.hide(Item.of('structure_gel:' + element).ignoreNBT())
-  });
-
-
-  //RFT Tools
-  const remove = [
-    'advanced_charged_porter',
-    'charged_porter',
-    'teleport_probe',
-    'creative_screen',
-    'matter_booster',
-    'tank',
-    'matter_beamer',
-    'flight_module',
-    'luck_module',
-    'peaceful_module',
-    'spawner',
-    'destination_analyzer',
-    'dialing_device',
-    'matter_receiver',
-    'matter_transmitter',
-    'syringe'
-  ]
-
-  remove.forEach((element) => {
-    event.hide (Item.of('rftoolsutility:' + element).ignoreNBT())
-  });
-
-  //Industrial Foregoing
-  const infinty_hide = [
-    'saw',
-    'hammer',
-    'trident',
-    'backpack',
-    'launcher',
-    'nuke',
-    'infinity_drill',
-    'drill'
-  ]
-
-  infinty_hide.forEach((element) => {
-    event.hide (
-      Item.of('industrialforegoing:infinity_' + element).ignoreNBT()
-    )
-  });
+    mob_grinding.forEach((element) => {
+        event.hide(Item.of('mob_grinding_utils:' + element).ignoreNBT())
+    });
 
 
-  //alltheores - Other Ores werden removed
-  const other_oreToRemove = [
-    "aluminum",
-    "lead",
-    "nickel",
-    "osmium",
-    "platinum",
-    "silver",
-    "tin",
-    "uranium",
-    "zinc",
-    "iridium",
-    "coal",
-    "copper",
-    "emerald",
-    "iron",
-    "gold",
-    "quartz",
-    "redstone",
-    "diamond",
-    "lapis",
-  ]
+    //Structure_gel API Build Mod
+    const structure_api = [
+        'dynamic_spawner',
+        'red_gel',
+        'blue_gel',
+        'green_gel',
+        'cyan_gel',
+        'orange_gel',
+        'building_tool',
+        'yellow_gel',
+        'data_handler'
+    ]
 
-  other_oreToRemove.forEach((atm_ore) => {
-      event.hide(`alltheores:other_${atm_ore}_ore`)
-      event.hide(Item.of('ae2:facade', "{item:\"alltheores:other_" + atm_ore + "_ore\"}"
-    ))
-  })
+    structure_api.forEach((element) => {
+        event.hide(Item.of('structure_gel:' + element).ignoreNBT())
+    });
 
-  event.hide(Item.of('mekanism:creative_chemical_tank')).ignoreNBT()
+
+    //RFT Tools
+    const remove = [
+        'advanced_charged_porter',
+        'charged_porter',
+        'teleport_probe',
+        'creative_screen',
+        'matter_booster',
+        'tank',
+        'matter_beamer',
+        'flight_module',
+        'luck_module',
+        'peaceful_module',
+        'spawner',
+        'destination_analyzer',
+        'dialing_device',
+        'matter_receiver',
+        'matter_transmitter',
+        'syringe'
+    ]
+
+    remove.forEach((element) => {
+        event.hide(Item.of('rftoolsutility:' + element).ignoreNBT())
+    });
+
+    //Industrial Foregoing
+    const infinty_hide = [
+        'saw',
+        'hammer',
+        'trident',
+        'backpack',
+        'launcher',
+        'nuke',
+        'infinity_drill',
+        'drill'
+    ]
+
+    infinty_hide.forEach((element) => {
+        event.hide(
+            Item.of('industrialforegoing:infinity_' + element).ignoreNBT()
+        )
+    });
+
+
+    //alltheores - Other Ores werden removed
+    const other_oreToRemove = [
+        'aluminum',
+        'lead',
+        'nickel',
+        'osmium',
+        'platinum',
+        'silver',
+        'tin',
+        'uranium',
+        'zinc',
+        'iridium',
+        'coal',
+        'copper',
+        'emerald',
+        'iron',
+        'gold',
+        'quartz',
+        'redstone',
+        'diamond',
+        'lapis',
+    ]
+
+    other_oreToRemove.forEach((atm_ore) => {
+        event.hide(`alltheores:other_${ atm_ore }_ore`)
+        event.hide(Item.of('ae2:facade', '{item:"alltheores:other_' + atm_ore + '_ore"}'
+        ))
+    })
+
+    event.hide(Item.of('mekanism:creative_chemical_tank')).ignoreNBT()
 })
