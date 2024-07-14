272d0d9514a4804d09497b934769a5950ebd64d3
Radon8472 <develop@radon-software.net>
2024-07-14 22:19:01 +0200

    Staging backpack upgrades

--- kubejs/server_scripts/gated/sophisticatedbackpacks.js
+++ kubejs/server_scripts/gated/sophisticatedbackpacks.js
@@ -15,4 +15,20 @@ ServerEvents.recipes(event => {
         1, 2, {'item': stages.stage3.core}
     )
 
+
+    // Exp upgrade limited to stage 3
+    replacer.replaceInputByPosition(
+        {id: 'sophisticatedbackpacks:xp_pump_upgrade'},
+        1, 2, {'item': stages.stage2.core}
+    )
+
+    // 'sophisticatedbackpacks:everlasting_upgrade'
+
+    // Potential endgame item - Singularity was not working
+    event.replaceInput(
+        'sophisticatedbackpacks:inception_upgrade',
+        // 'minecraft:diamond', Item.of('extendedcrafting:singularity', '{Id:"extendedcrafting:diamond"}')
+        'minecraft:diamond', 'extendedcrafting:crystaltine_ingot'
+    );
+
 })
