2c33c2c5f3ca1b6f01b9d7cf506a3489f796dc8f
zukkl <mrozikrobin@gmail.com>
2024-07-20 14:07:44 +0200

    clear adastra skript and remove doubles

--- kubejs/server_scripts/gated/adastra.js
+++ kubejs/server_scripts/gated/adastra.js
@@ -6,112 +6,71 @@ ServerEvents.recipes(event => {
 
     // Coal generator
     event.replaceInput(
-        { id: 'ad_astra:recipes/coal_generator' },         // Filterung nach Rezept-ID.
-        'minecraft:iron_ingot',            // Das Item, was ersetzt werden soll.
-        stages.stage1.core                 // Womit wird das Item ersetzt!
+        { id: 'ad_astra:recipes/coal_generator' },
+        'minecraft:iron_ingot',
+        stages.stage1.core
     )
 
     // Solar Panel
     event.replaceInput(
-        { id: 'ad_astra:recipes/solar_panel' },         // Filterung nach Rezept-ID.
-        'minecraft:blue_stained_glass',            // Das Item, was ersetzt werden soll.
-        stages.stage1.core                 // Womit wird das Item ersetzt!
+        { id: 'ad_astra:recipes/solar_panel' },
+        'minecraft:blue_stained_glass',
+        stages.stage1.core
+    )
+
+    // Cable Duct
+    event.replaceInput(
+        { id: 'ad_astra:recipes/cable_duct' },
+        'minecraft:copper_ingot',
+        stages.stage1.core
     )
 
     // Nasa Workbench
     event.replaceInput(
-        { id: 'ad_astra:recipes/nasa_workbench' },         // Filterung nach Rezept-ID.
-        'minecraft:redstone_block',            // Das Item, was ersetzt werden soll.
-        stages.stage1.core                 // Womit wird das Item ersetzt!
+        { id: 'ad_astra:recipes/nasa_workbench' },
+        'minecraft:redstone_block',
+        stages.stage1.core
     )
 
     // Energizer
     event.replaceInput(
-        { id: 'ad_astra:recipes/energizer' },         // Filterung nach Rezept-ID.
-        'minecraft:diamond',            // Das Item, was ersetzt werden soll.
-        stages.stage1.core                 // Womit wird das Item ersetzt!
-    )
-
-    // Cable Duct
-    event.replaceInput(
-        { id: 'ad_astra:recipes/cable_duct' },         // Filterung nach Rezept-ID.
-        'minecraft:copper_ingot',            // Das Item, was ersetzt werden soll.
-        stages.stage1.core                 // Womit wird das Item ersetzt!
+        { id: 'ad_astra:recipes/energizer' },
+        'minecraft:diamond',
+        stages.stage1.core
     )
 
     // Fluid Pipe Duct
     event.replaceInput(
-        { id: 'ad_astra:recipes/fluid_pipe_duct' },         // Filterung nach Rezept-ID.
-        'minecraft:glass',            // Das Item, was ersetzt werden soll.
-        stages.stage1.core                 // Womit wird das Item ersetzt!
+        { id: 'ad_astra:recipes/fluid_pipe_duct' },
+        'minecraft:glass',
+        stages.stage1.core
     )
     // Desh Fluid Pipe
     event.replaceInput(
-        { id: 'ad_astra:recipes/desh_fluid_pipe' },         // Filterung nach Rezept-ID.
-        'minecraft:glass',            // Das Item, was ersetzt werden soll.
-        stages.stage1.core                 // Womit wird das Item ersetzt!
+        { id: 'ad_astra:recipes/desh_fluid_pipe' },
+        'minecraft:glass',
+        stages.stage1.core
     )
 
     // Ostrum Fluid Pipe
     event.replaceInput(
-        { id: 'ad_astra:recipes/ostrum_fluid_pipe' },         // Filterung nach Rezept-ID.
-        'minecraft:glass',            // Das Item, was ersetzt werden soll.
-        stages.stage1.core                 // Womit wird das Item ersetzt!
-    )
-
-        // Nasa Workbench
-    event.replaceInput(
-        { id: 'ad_astra:recipes/nasa_workbench' },         // Filterung nach Rezept-ID.
-        'minecraft:redstone_block',            // Das Item, was ersetzt werden soll.
-        stages.stage1.core                 // Womit wird das Item ersetzt!
-    )
-
-    // Energizer
-    event.replaceInput(
-        { id: 'ad_astra:recipes/energizer' },         // Filterung nach Rezept-ID.
-        'minecraft:diamond',            // Das Item, was ersetzt werden soll.
-        stages.stage1.core                 // Womit wird das Item ersetzt!
-    )
-
-    // Cable Duct
-    event.replaceInput(
-        { id: 'ad_astra:recipes/cable_duct' },         // Filterung nach Rezept-ID.
-        'minecraft:copper_ingot',            // Das Item, was ersetzt werden soll.
-        stages.stage1.core                 // Womit wird das Item ersetzt!
-    )
-
-    // Fluid Pipe Duct
-    event.replaceInput(
-        { id: 'ad_astra:recipes/fluid_pipe_duct' },         // Filterung nach Rezept-ID.
-        'minecraft:glass',            // Das Item, was ersetzt werden soll.
-        stages.stage1.core                 // Womit wird das Item ersetzt!
-    )
-    // Desh Fluid Pipe
-    event.replaceInput(
-        { id: 'ad_astra:recipes/desh_fluid_pipe' },         // Filterung nach Rezept-ID.
-        'minecraft:glass',            // Das Item, was ersetzt werden soll.
-        stages.stage1.core                 // Womit wird das Item ersetzt!
-    )
-
-    // Ostrum Fluid Pipe
-    event.replaceInput(
-        { id: 'ad_astra:recipes/ostrum_fluid_pipe' },         // Filterung nach Rezept-ID.
-        'minecraft:glass',            // Das Item, was ersetzt werden soll.
-        stages.stage1.core                 // Womit wird das Item ersetzt!
+        { id: 'ad_astra:recipes/ostrum_fluid_pipe' },
+        'minecraft:glass',
+        stages.stage1.core
     )
 
     // Fuel Refinery
     event.replaceInput(
-        { id: 'ad_astra:recipes/fuel_refinery' },         // Filterung nach Rezept-ID.
-        'minecraft:bucket',            // Das Item, was ersetzt werden soll.
-        stages.stage1.core                 // Womit wird das Item ersetzt!
+        { id: 'ad_astra:recipes/fuel_refinery' },
+        'minecraft:bucket',
+        stages.stage1.core
     )
 
     // Oxygen Loader
     event.replaceInput(
-        { id: 'ad_astra:recipes/oxygen_loader' },         // Filterung nach Rezept-ID.
-        'minecraft:redstone_block',            // Das Item, was ersetzt werden soll.
-        stages.stage1.core                 // Womit wird das Item ersetzt!
+        { id: 'ad_astra:recipes/oxygen_loader' },
+        'minecraft:redstone_block',
+        stages.stage1.core
     )
 
 
