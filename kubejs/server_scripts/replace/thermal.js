12b0add6816a64f0885667c68c3ddd626b73ed74
Radon8472 <develop@radon-software.net>
2024-08-16 23:05:49 +0200

    replace bamboo_plank recipe - with input by tags
    - 2x blocks => 4x planks instead of 1x block => 4x planks

--- kubejs/server_scripts/replace/thermal.js
+++ kubejs/server_scripts/replace/thermal.js
@@ -4,7 +4,7 @@ ServerEvents.recipes(event => {
     event.shapeless('9x minecraft:bamboo', [ 'minecraft:bamboo_block' ])
 
     // replace old planks recipe
-    event.shapeless('4x minecraft:bamboo_planks', [ '2x minecraft:bamboo_block' ])
+    event.shapeless('4x minecraft:bamboo_planks', [ '2x #minecraft:bamboo_blocks' ])
     event.remove({ id: 'minecraft:bamboo_planks' })
 
 })
