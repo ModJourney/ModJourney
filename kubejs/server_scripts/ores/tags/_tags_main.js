939f8ef89f8af74152cf82c9d38be8e6e628b3a7
zukkl <mrozikrobin@gmail.com>
2024-08-21 05:50:45 +0200

    readd Tag Builders and build single Handler Main.js

---
+++ kubejs/server_scripts/ores/tags/_tags_main.js
@@ -0,0 +1,12 @@
+//priority 0
+
+//sammelstelle zum entfernen der Tags, nachdem alle Rezepthändler durchgelaufen sind.
+//Kann durch auskommentieren bedarfsweise ausgeschaltet werden.
+ServerEvents.tags('item', event => {
+
+    //ores_ad_astra_tag(event)
+    //ores_meka_tag(event)
+    //ores_immersive_tag(event)
+    //ores_thermal_tag(event)
+
+})
--- kubejs/server_scripts/ores/tags/global_tag.js
+++ kubejs/server_scripts/ores/tags/global_tag.js
@@ -1,13 +1,13 @@
 //priority 5
 
 /**
- * Added das ItemTag ore:remove und ore:remove/Itemid
+ * Added das ItemTag ore:remove
  * Muss im eventTag Handler definiert werden.
- * @param {String} item_id Gibt die Eindeutige ItemId an.
+ * @param {String} item_id Eindeutige ItemId an.
  * @example ServerEvents.tags(event,'item', event => {
  *  add_removeTag("minecraft:stone")
  * })
- * add >> ore:remove and ore:remove/stone
+ * add >> ore:remove
  */
 let add_removeTag = (event, item_id) => {
     event.removeAllTagsFrom(item_id)
