8408af7a446066c5e29d95d7ac84e7003ccb6b87
Radon8472 <develop@radon-software.net>
2024-09-08 01:40:16 +0200

    #128 Add oil to foregoing fluid laser
    - see also https://github.com/InnovativeOnlineIndustries/Industrial-Foregoing/blob/1.20/src/generated/resources/data/industrialforegoing/recipes/laser_drill_fluid/oil.json

--- kubejs/server_scripts/add/mod_journey/scrolls.js
+++ kubejs/server_scripts/add/mod_journey/scrolls.js
@@ -34,4 +34,35 @@ event.shaped(
     ]
   ).id("modjourney:scroll/9")
 
+
+    event.custom({
+        "type": "industrialforegoing:laser_drill_fluid",
+        "catalyst": {
+        "item": "industrialforegoing:laser_lens2"
+    },
+        "entity": "minecraft:empty",
+        "output": "{Amount:100,FluidName:\"minecraft:water\"}",
+        "pointer": 0,
+        "rarity": [
+        {
+            "blacklist": {},
+            "depth_max": 65,
+            "depth_min": 5,
+            "weight": 8,
+            "whitelist": {
+                "type": "minecraft:worldgen/biome",
+                "values": [
+                    "minecraft:ocean",
+                    "minecraft:warm_ocean",
+                    "minecraft:lukewarm_ocean",
+                    "minecraft:frozen_ocean",
+                    "minecraft:deep_ocean",
+                    "minecraft:deep_warm_ocean",
+                    "minecraft:deep_lukewarm_ocean",
+                    "minecraft:deep_frozen_ocean"
+                ]
+            }
+        }
+    ]
+    })
 })
