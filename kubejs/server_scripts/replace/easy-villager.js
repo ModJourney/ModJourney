3d901eabefeb62dc33d1c17791236005ed575510
zukkl <mrozikrobin@gmail.com>
2024-09-08 20:10:53 +0200

    #115, #106, change easy_villager iron farm

---
+++ kubejs/server_scripts/replace/easy-villager.js
@@ -0,0 +1,8 @@
+ServerEvents.recipes(event => {
+
+    event.replaceInput(
+        {output: "easy_villagers:iron_farm"},
+        "minecraft:stone",
+        "#forge:storage_blocks/steel"
+    )
+})
