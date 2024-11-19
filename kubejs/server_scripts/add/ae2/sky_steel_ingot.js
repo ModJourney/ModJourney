527f1b6a1e515797d2a05c07405d62ce7c35c19d
Radon8472 <develop@radon-software.net>
2024-11-19 13:26:38 +0100

    Add automatic sky steel ingot recipe for infuser

--- kubejs/server_scripts/add/ae2/sky_steel_ingot.js
+++ kubejs/server_scripts/add/ae2/sky_steel_ingot.js
@@ -2,6 +2,25 @@ ServerEvents.recipes(event => {
 
     event.custom(
         {
+            "type": "mekanism:metallurgic_infusing",
+            "chemicalInput": {
+                "amount": 120,
+                "tag": "mekanism:refined_obsidian"
+            },
+            "itemInput": {
+                "ingredient": {
+                    "item" : "ae2:sky_stone_block"
+                }
+            },
+            "output": {
+                "count": 1,
+                "item": "megacells:sky_steel_ingot"
+            }
+        }
+    ).id("mod_journey:infusing/sky_steel_ingot")
+
+    event.custom(
+        {
             "type": "industrialforegoing:dissolution_chamber",
             "input": [
                 {
